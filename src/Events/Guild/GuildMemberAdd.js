112aa6f7b262d7002fbd02c73cae623c56989ccf
Kevin C <webmasterpowerzone@gmail.com>
2021-03-09 19:03:49 +0100

    Create GuildMemberAdd.js
    New event for this project
    
    Name: GuildMemberAdd
    
    Description: Welcome somebody in server..

---
+++ src/Events/Guild/GuildMemberAdd.js
@@ -0,0 +1,19 @@
+const {BaseEvent} = require('../../Utils/Structures/index.js');
+
+const Discord = require('discord.js');
+
+module.exports = class GuildMemberAdd extends BaseEvent {
+    super({
+      name: 'GuildMemberAdd',
+      enabled: 'true',
+      once: true
+    })
+}
+
+  async run (client) {
+    client.on("guildMemberAdd", (member) => {
+      const welcomeChannel = message.guild.channels.find(c => c.name === 'welcome');
+      message.channel.send("Welcome into our server, \n We hope you enjoy your stay with us");
+    })
+  }
+}
